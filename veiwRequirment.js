dab1dd85c951c8508113fa08afe2d95035b06cb6
MD MEHEDI HASAN <143672978+mehedi0105@users.noreply.github.com>
2024-10-03 22:14:55 +0600

    Update veiwRequirment.js

--- veiwRequirment.js
+++ veiwRequirment.js
@@ -1,86 +1,81 @@
-window.onload = () => {
-    alert();
-    const id = localStorage.getItem('view_Requ');
+// alert()
+window.onload = () =>{
+    const id = localStorage.getItem('view_Requ_id');
     const parent = document.getElementById("job_main");
     const token = localStorage.getItem("token");
-
-    // Clear the parent container if it has any content
     if (parent.innerHTML !== "") {
         parent.innerHTML = "";
     }
-
-    // Fetch job requirements
+    
     fetch(`https://final-s1v0.onrender.com/seller/project_requirment/`)
-        .then((res) => res.json())
-        .then(async (data) => {
-            console.log(parent);
-            let cnt = 0;
-
-            // Use a for...of loop to handle async operations within the loop
-            for (const element of data) {
-                console.log(element.id, "  ", id);
-
-                // Ensure only the matching job ID is processed once
-                if (String(element.job) === id && cnt === 0) {
+    .then((res) => res.json())
+    .then(async (data) => {
+        {
+            let cnt =0 ;
+            data.forEach(async(element) => {
+                console.log(element.id,"  ",id)
+                if(String(element.id) == id && cnt === 0){
                     cnt++;
-                    const company_name = await getIdSendUsername(element.buyer);
-                    const tittle = await getIdSendTittle(element.job);
+                const company_name = await getIdSendUsername(element.buyer);
+                const tittle = await getIdSendTittle(element.job);
+                const div = document.createElement("div");
+            div.innerHTML = `
+                <div class="container my-5">
 
-                    // Create the job overview and insert it into the DOM
-                    const div = document.createElement("div");
-                    div.innerHTML = `
-                        <div class="container my-5">
-                            <div class="card top-box mt-4" style="border: none">
-                                <div class="card-body top-box-body d-flex">
-                                    <div class="mr-4">
-                                        <img class="company_logo"
-                                             src="https://workscout.in/wp-content/uploads/job-manager-uploads/company_logo/2021/11/company-logo-03-150x150.png"
-                                             height="117px;" alt=""/>
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <h5>${company_name}</h5>
-                                        <p class="text-muted">${company_name} IT Solution</p>
-                                        <div class="d-flex mb-3">
-                                            <a href="#" class="btn btn-light btn-sm mr-2">🌐 Website</a>
-                                            <a href="#" class="btn btn-light btn-sm mr-2">✉️ ${company_name}@test.com</a>
-                                            <a href="#" class="btn btn-light btn-sm mr-2 job-details-btn">🐦 Twitter</a>
-                                            <a href="#" class="btn btn-light btn-sm mr-2 job-details-btn">📘 Facebook</a>
-                                            <a href="#" class="btn btn-light btn-sm job-details-btn">📞 123456789</a>
-                                        </div>
-                                    </div>
-                                    <div>
-                                      <button type="button" class="btn text-white job-details-btn" onclick="SaveSubmitRequirmentData(${element.job})" style="background-color: #26ae61; padding: 15px;" data-bs-toggle="modal" data-bs-target="#applyModal">Accept Job</button>
-                                    </div>
+                    <div class="card top-box mt-4" style="border: none">
+                        <div class="card-body top-box-body d-flex">
+                            <div class="mr-4">
+                                <img class="company_logo"
+                                     src="https://workscout.in/wp-content/uploads/job-manager-uploads/company_logo/2021/11/company-logo-03-150x150.png"
+                                     height="117px;" alt=""/>
+                            </div>
+                            <div class="flex-grow-1">
+                                <h5>${company_name}</h5>
+                                <p class="text-muted">${company_name} It Soluation</p>
+                                <div class="d-flex mb-3">
+                                    <a href="#" class="btn btn-light btn-sm mr-2">🌐 Website</a>
+                                    <a href="#" class="btn btn-light btn-sm mr-2">✉️ ${company_name}@test.com</a>
+                                    <a href="#" class="btn btn-light btn-sm mr-2 job-details-btn">🐦 Twitter</a>
+                                    <a href="#" class="btn btn-light btn-sm mr-2 job-details-btn">📘 Facebook</a>
+                                    <a href="#" class="btn btn-light btn-sm job-details-btn">📞 123456789</a>
+                                </div>
+                            </div>
+                            <div>
+                              <button type="button" class="btn text-white job-details-btn" onclick="SaveSubmitRequirmentData(${element.job})" class="btn text-white" style="background-color: #26ae61; padding: 15px;" data-bs-toggle="modal" data-bs-target="#applyModal">Accept Job</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="container my-5">
+                    <div class="row">
+                        
+                        <div class="col-lg-12">
+                            <div class="card" style="background: #fdfdfd;border: none;">
+                                <div class="card-body">
+                                    <h5 class="card-title">Job Requirment Overview</h5>
+                                    <ul class="list-unstyled job-overview">
+                                        <li><i class="fas fa-briefcase job-overview-icon"></i> Job Title: <br>
+                                            <span class="text-muted" style="margin-left: 46px;">${tittle}</span>
+                                        </li>
+                                        <li><i class="fa-solid fa-envelope job-overview-icon"></i>Requirment: <br>
+                                        <p style="margin-left: 46px;" class="Description">${element.requirment}</p>
+                                        </li>
+                                    </ul>
+                                    <button type="button" class="btn text-white w-100" onclick="SaveSubmitRequirmentData(${element.job})" class="btn text-white" style="background-color: #26ae61; padding: 15px" data-bs-toggle="modal" data-bs-target="#applyModal">Accept Job</button>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="container my-5">
-                            <div class="row">
-                                <div class="col-lg-12">
-                                    <div class="card" style="background: #fdfdfd; border: none;">
-                                        <div class="card-body">
-                                            <h5 class="card-title">Job Requirement Overview</h5>
-                                            <ul class="list-unstyled job-overview">
-                                                <li><i class="fas fa-briefcase job-overview-icon"></i> Job Title: <br>
-                                                    <span class="text-muted" style="margin-left: 46px;">${tittle}</span>
-                                                </li>
-                                                <li><i class="fa-solid fa-envelope job-overview-icon"></i>Requirement: <br>
-                                                <p style="margin-left: 46px;" class="Description">${element.requirment}</p>
-                                                </li>
-                                            </ul>
-                                            <button type="button" class="btn text-white w-100" onclick="SaveSubmitRequirmentData(${element.job})" style="background-color: #26ae61; padding: 15px;" data-bs-toggle="modal" data-bs-target="#applyModal">Accept Job</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    `;
-                    parent.appendChild(div);
-                }
+                    </div>
+                </div>
+            `;
+            parent.appendChild(div);
             }
         });
-};
+               
+        }
+    });
+}
 
 
 
